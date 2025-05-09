2b9bd14b6e3c66303e0c63180f49c50d379f6233
Shiv-am-04 <shivamghuge01@gmail.com>
2025-05-09 22:28:39 +0530

    feature_engineering step completed successfully

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import sys
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_preprocessing import DataPreprocessing
+from src.components.feature_engineering import FeatureEngineering
 import pandas as pd
 
 # checking error handling and logging
@@ -16,20 +17,30 @@ import pandas as pd
 
 # checking data ingestion
 
-data = pd.read_csv(r'DS-Intern-Assignment-Shivam-Ghuge\data\data.csv')
+# data = pd.read_csv(r'DS-Intern-Assignment-Shivam-Ghuge\data\data.csv')
 
-ingestion = DataIngestion(data=data)
+# ingestion = DataIngestion(data=data)
 
-train,test = ingestion.split_data()
-ingestion.export_data(train,test)
+# train,test = ingestion.split_data()
+# ingestion.export_data(train,test)
 
-# checking data prerocessing
+# # checking data prerocessing
 
-train_path = r'DS-Intern-Assignment-Shivam-Ghuge\data\splited\train.csv'
-test_path = r'DS-Intern-Assignment-Shivam-Ghuge\data\splited\test.csv'
+# train_path = r'DS-Intern-Assignment-Shivam-Ghuge\data\splited\train.csv'
+# test_path = r'DS-Intern-Assignment-Shivam-Ghuge\data\splited\test.csv'
 
-preprocessor = DataPreprocessing(train_path,test_path)
+# preprocessor = DataPreprocessing(train_path,test_path)
 
-train_scaled,test_scaled = preprocessor.preprocessing()
+# train_scaled,test_scaled = preprocessor.preprocessing()
 
-preprocessor.export_data(train_scaled,test_scaled)
+# preprocessor.export_data(train_scaled,test_scaled)
+
+# checking feature_engineering
+
+train_ = pd.read_csv(r'DS-Intern-Assignment-Shivam-Ghuge\data\preprocessed\train.csv')
+test_ = pd.read_csv(r'DS-Intern-Assignment-Shivam-Ghuge\data\preprocessed\test.csv')
+
+fe = FeatureEngineering(train_,test_)
+fe.feature_handeling()
+train_data,test_data = fe.encoding_feature()
+fe.export_data(train_data,test_data)
---
+++ src/components/feature_engineering.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+from src.exception import CustomException
+from src.logger import logging
+
+
+class FeatureEngineering:
+    def __init__(self,train_data:str,test_data:str):
+
+        try:
+            self.train = train_data
+            self.test = test_data
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def feature_handeling(self):
+        # converting to datetime datatype because the datatype of timestamp feature is object
+        try:
+            self.train['timestamp'] = pd.to_datetime(self.train['timestamp'])
+            self.test['timestamp'] = pd.to_datetime(self.test['timestamp'])
+            logging.info("converted 'timestamp' feature to datetime dataype")
+
+            # creating new features
+            self.train['hour'] = self.train['timestamp'].dt.hour
+            self.train['minute'] = self.train['timestamp'].dt.minute
+            self.train['month'] = self.train['timestamp'].dt.month
+            self.train['day_of_week'] = self.train['timestamp'].dt.dayofweek
+
+            self.test['hour'] = self.test['timestamp'].dt.hour
+            self.test['minute'] = self.test['timestamp'].dt.minute
+            self.test['month'] = self.test['timestamp'].dt.month
+            self.test['day_of_week'] = self.test['timestamp'].dt.dayofweek
+            logging.info("new features : [hour,minute,day_of_week,month] created using 'timestamp' feature for both train and test dataset")
+        except Exception as e:
+            raise CustomException(e,sys)
+
+        
+    def encoding_feature(self):
+        try:
+            self.train['hour_sin'] = np.sin(2 * np.pi * self.train['hour'] / 24)
+            self.train['hour_cos'] = np.cos(2 * np.pi * self.train['hour'] / 24)
+
+            # Minute of the hour (0 to 59)
+            self.train['minute_sin'] = np.sin(2 * np.pi * self.train['minute'] / 60)
+            self.train['minute_cos'] = np.cos(2 * np.pi * self.train['minute'] / 60)
+
+            # Day of the week (0 to 6)
+            self.train['day_of_week_sin'] = np.sin(2 * np.pi * self.train['day_of_week'] / 7)
+            self.train['day_of_week_cos'] = np.cos(2 * np.pi * self.train['day_of_week'] / 7)
+
+            # Month of the year (1 to 12)
+            self.train['month_sin'] = np.sin(2 * np.pi * (self.train['month'] - 1) / 12)
+            self.train['month_cos'] = np.cos(2 * np.pi * (self.train['month'] - 1) / 12)
+            logging.info("encoded train data")
+
+            self.train.drop(columns=['timestamp','hour','minute','day_of_week','month'],inplace=True)
+            logging.info("columns : ['timestamp','hour','minute','day_of_week','month'] droped from train dataset")
+        except Exception as e:
+            raise CustomException(e,sys)
+
+        try:
+            # for test dataset
+            self.test['hour_sin'] = np.sin(2 * np.pi * self.test['hour'] / 24)
+            self.test['hour_cos'] = np.cos(2 * np.pi * self.test['hour'] / 24)
+
+            # Minute of the hour (0 to 59)
+            self.test['minute_sin'] = np.sin(2 * np.pi * self.test['minute'] / 60)
+            self.test['minute_cos'] = np.cos(2 * np.pi * self.test['minute'] / 60)
+
+            # Day of the week (0 to 6)
+            self.test['day_of_week_sin'] = np.sin(2 * np.pi * self.test['day_of_week'] / 7)
+            self.test['day_of_week_cos'] = np.cos(2 * np.pi * self.test['day_of_week'] / 7)
+
+            # Month of the year (1 to 12)
+            self.test['month_sin'] = np.sin(2 * np.pi * (self.test['month'] - 1) / 12)
+            self.test['month_cos'] = np.cos(2 * np.pi * (self.test['month'] - 1) / 12)
+            logging.info("encoded test data")
+
+            self.test.drop(columns=['timestamp','hour','minute','day_of_week','month'],inplace=True)
+            logging.info("columns : ['timestamp','hour','minute','day_of_week','month'] droped from test dataset")
+        except Exception as e:
+            raise CustomException(e,sys)
+
+        return self.train,self.test
+        
+    def export_data(self,train:pd.DataFrame,test:pd.DataFrame):
+        try:
+            path = 'DS-Intern-Assignment-Shivam-Ghuge\data\FE_data'
+            os.makedirs(path,exist_ok=True)
+            train.to_csv(os.path.join(path,'train.csv'),header=True,index=False)
+            test.to_csv(os.path.join(path,'test.csv'),header=True,index=False)
+            logging.info(f"train and test data saved to {os.path.split(path)[-1]} folder")
+        except Exception as e:
+            raise CustomException(e,sys)
